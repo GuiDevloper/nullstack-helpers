08211da58264d30f6713e0df8d949e67c9153f0a
GuiDevloper <guicorrea384@gmail.com>
2021-11-30 17:28:53 -0300

    Document types and organize files

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="nullstack-types"/>
 
+/**
+ * Generates a class component based on params
+ */
 export function createApp(params: {
   methods?: {
     initiate?(context: Context)
@@ -10,3 +13,49 @@ export function createApp(params: {
   },
   render?(context: Context, components: Object)
 })
+
+type ElementNode = {
+  type: String | Boolean,
+  attributes: {
+    route: String,
+    html: String
+  },
+  children: Array<any>
+};
+
+interface ContextNode extends Context {
+  node: ElementNode
+}
+
+export function createPlugin(params: {
+  /**
+   * Runs transformation to node element
+   * @param context Context with node attributes
+   */
+  transform?(context: ContextNode),
+  /**
+   * Load something when plugin installs
+   * @param context Application context
+   */
+  load?(context: Context),
+  /**
+   * Use plugin in server context
+   */
+  server?: Boolean,
+  /**
+   * Use plugin in client context
+   */
+  client?: Boolean
+}): Object
+
+/**
+ * Generates a function to match a specific node
+ * @param fn Function testing the node
+ */
+export function pluginMatch(fn: function (ElementNode)): (function(): Boolean)
+
+/**
+ * Generates a function to be used as class method
+ * @param fn Pure class method
+ */
+export function classFn(fn: function (Context)): (function(): any)
