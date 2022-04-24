0fd3f14886ea6b78dfea0059145717282f9fae62
GuiDevloper <guicorrea384@gmail.com>
2022-04-24 19:19:40 -0300

    :arrow_up: Use new nullstack-types

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,15 @@
-/// <reference types="nullstack-types"/>
+import type { NullstackNode, NullstackPlugin } from 'nullstack-types';
 
 /**
  * Generates a plugin object
  * @param params Plugin object
  */
-export function createPlugin(params: ElementPlugin): ElementPlugin
+export function createPlugin(params: NullstackPlugin): NullstackPlugin
 
 /**
  * Generates a function to match a specific node
  * @param fn Function testing the node
  */
 export function pluginMatch(
-  fn: (node: ElementNode) => any
-): (node: ElementNode) => boolean
+  fn: (node: NullstackNode) => any
+): (node: NullstackNode) => boolean
